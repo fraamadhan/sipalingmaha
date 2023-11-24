07fab2229ee06a50a974c863d8f060a35159d5c0
fraamadhan <fakhrifajarrr@gmail.com>
2023-11-24 23:37:39 +0700

    feat: integrated crud with UI

--- app/dashboard/addTopic/page.tsx
+++ app/dashboard/addTopic/page.tsx
@@ -1,21 +1,63 @@
+'use client'
+
 import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function AddTopic() {
+  const [title, settitle] = useState("")
+  const [description, setdescription] = useState("")
+  const router = useRouter()
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Title and description are required")
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:3000/api/topics', {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({title, description})
+      });
+
+      if (res.ok) {
+        router.push('/dashboard')
+        alert("Topic added successfully")
+      } else {
+        throw new Error("Failed to create a topic")
+      }
+    } catch(err) {
+        console.log(err);
+        
+    }
+
+  }
+
   return (
     <div className='mt-28 p-4'>
-      <form className='flex flex-col gap-3 w-full'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-3 w-full text-black'>
         <input
           className='border border-slate-500 px-8 py-2 w-full rounded-xl'
           type='text'
           placeholder='Topic Title'
+          value={title}
+          onChange={(e) => settitle(e.target.value)}
         />
         <input
+          onChange={(e) => setdescription(e.target.value)}
+          value={description}
           className='border border-slate-500 px-8 py-2 w-full rounded-xl'
           type='text'
           placeholder='Topic Description'
         />
-        <button className='bg-green-600 font-bold text-white py-3 px-6 w-fit rounded-xl'>
-            Add Topic
+        <button type='submit' className='bg-green-600 font-bold text-white py-3 px-6 w-fit rounded-xl'>
+          Add Topic
         </button>
       </form>
     </div>
--- app/dashboard/editTopic/[id]/page.tsx
+++ app/dashboard/editTopic/[id]/page.tsx
@@ -1,10 +1,30 @@
 import EditTopicForm from '@/app/components/EditTopicForm'
 import React from 'react'
 
-export default function editTopic() {
+const getTopicById = async(id: any) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      cache: 'no-store',
+    });
+
+    if (!res.ok) {
+      throw new Error("Data failed to update");
+    }
+
+    return res.json()
+  } catch (err) {
+    console.log(err);
+    
+  }
+}
+
+export default async function editTopic({params}: any) {
+  const {id} = params;
+  const {topic} = await getTopicById(id);
+  const {topicTitle, topicDescription} = topic;
   return (
     <div className='mt-28 p-4'>
-      <EditTopicForm/>
+      <EditTopicForm id={id} title={topicTitle} description={topicDescription}/>
     </div>
   )
 }
